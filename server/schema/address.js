cac81a3554993f6c296d4e256aaf677693bea903
Harsh Vakharia <harshjv@gmail.com>
2016-11-18 23:46:12 +0530

    Update domain validation

--- server/schema/address.js
+++ server/schema/address.js
@@ -7,7 +7,7 @@ const schema = Joi.object().keys({
   name: Joi.string().min(2).required().label('Name'),
   email: Joi.string().email().required().label('Email'),
   cluster: Joi.string().valid(clusters).required().label('Cluster'),
-  domain: Joi.string().invalid(reservedAddresses).min(5).max(63).insensitive().required().label('Domain'),
+  domain: Joi.string().invalid(reservedAddresses).alphanum().min(5).max(63).insensitive().required().label('Domain'),
   subscribe: Joi.boolean().default(false)
 })
 
